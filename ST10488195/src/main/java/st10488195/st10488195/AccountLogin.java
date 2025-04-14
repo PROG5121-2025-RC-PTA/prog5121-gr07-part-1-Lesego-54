d17fcc620fdfd985953f9ceeaa01b6384c8da470
Lesego Maake Moleele <moleelelesego@outlook.com>
2025-04-14 11:43:14 +0200

    Add files via upload

---
+++ ST10488195/src/main/java/st10488195/st10488195/AccountLogin.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package st10488195.st10488195;
+
+/**
+ *
+ * @author Molee
+ */import java.util.Scanner;
+public class AccountLogin {
+    public static void main (String[] args)
+    {
+  
+      Scanner Userinfo = new Scanner(System.in);
+      Boolean Username = true;
+      System.out.println("Username successfully captured.");
+      Boolean Password = true;
+      System.out.println("Password successfully captured.");
+      Boolean CellphoneNumber = true;
+      System.out.println("Cell phone number successfully added");
+      String registerUser;
+      System.out.println("Welcome < user fisrt name > , < it is great to see you again.");
+      System.out.print("Usename or password incorrect,please try again");
+      Boolean loginUser;
+      System.out.println("Welcome < user fisrt name > , < it is great to see you again.");
+      String returnLoginStatus;
+      System.out.println("Welcome < user fisrt name > , < it is great to see you again.");
+        System.out.print("Usename or password incorrect,please try again");
+      
+    }
+}
---
+++ ST10488195/src/main/java/st10488195/st10488195/UserLoginInfo.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package st10488195.st10488195;
+
+import java.util.Locale;
+/**
+ *
+ * @author Molee
+ */import java.util.Scanner;
+public class UserLoginInfo 
+{
+   public static void main (String[] args){
+       Scanner inputInfo = new Scanner(System.in);
+ 
+    char Username = '_';
+    System.out.println("Username successfully captured.");
+   if(Username>5);
+   System.out.println("Username is not correctly formatted,please ensure that you username contains an underscore and is no more than five characters in lenghth.");
+   
+   char charPassword;
+   int intPassword;
+   String letterPassword;
+   
+   System.out.println("Password successfully captured.");
+   char Password = 8;
+   System.out.print("Password successfully captured");
+   if (Password <8);
+   System.out.println("Password is not correctly formatted; please ensure that the password contain at least eight characters, a capital letter, a number, and a speacial character.");
+   
+   }
+ } 
+
