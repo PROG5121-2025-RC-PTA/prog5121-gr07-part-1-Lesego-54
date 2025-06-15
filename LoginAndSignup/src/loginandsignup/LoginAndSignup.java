1895be9f36aea067c2762e47df39ebe940c508b4
Lesego Maake Moleele <moleelelesego@outlook.com>
2025-06-15 16:11:30 +0200

    Add files via upload

---
+++ LoginAndSignup/src/loginandsignup/LoginAndSignup.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loginandsignup;
+
+public class LoginAndSignup {
+
+    public static void main(String[] args) {
+  
+        Login LoginFrame = new Login();
+LoginFrame.setVisible(true);
+LoginFrame.pack();
+LoginFrame.setLocationRelativeTo(null);
+
+    }
+    
+}
